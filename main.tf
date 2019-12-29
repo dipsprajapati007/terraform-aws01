8f46fe42d4af1cdc9e58f3342053287b9bbfbf37
dips.p.007@gmail.com <dips.p.007@gmail.com>
2019-12-29 13:26:19 +0800

    added git ignore

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 provider "aws" {
-    region = "us-east-2"
+    access_key = "AKIAQHO6AYTZ4NAX5DOM"
+    secret_key = "kQiNJ/KhyeX9dMzVT1MuYeiJASh2nnGvKcX9YAfl"
+    region = "us-east-1"
+}
 
+resource "aws_instance" "example" {
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
 }
