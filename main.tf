71faedf2ba4f4a9e10aa7a203ad44ed0dc8c614c
dips.p.007@gmail.com <dips.p.007@gmail.com>
2019-12-29 22:53:04 +0800

    keys removed

--- main.tf
+++ main.tf
@@ -1,14 +1,37 @@
 provider "aws" {
-    access_key = "AKIAQHO6AYTZ4NAX5DOM"
-    secret_key = "kQiNJ/KhyeX9dMzVT1MuYeiJASh2nnGvKcX9YAfl"
     region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
     ami = "ami-2757f631"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
 
     tags = {
         Name = "terraform-example"
     }
 }
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+}
+
